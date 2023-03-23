a3d3486a705c1be99e4c9fd9f783c8da1925d5b3
Junior Martini <erciliomartinijunior@gmail.com>
2023-03-23 14:41:17 -0400

    criado layout

--- password_gen.py
+++ password_gen.py
@@ -2,12 +2,32 @@ import random
 import PySimpleGUI as sg
 import os
 
+
 class PassGen:
     def __init__(self):
-        pass
+        # layout
+        sg.theme('Material2')
+        layout = [
+            [sg.text('Site/Software', size=(10, 1)),
+             sg.Input(key='site', size=(20, 1))],
+            [sg.text('E-mail/Usuário', size=(10, 1)),
+             sg.Input(key='usuario', size=(20, 1))],
+            [sg.text('Quantidade de caracteres'), sg.Combo(values=list(
+                range(30)),key='total_chars', default_value=1, size=(3, 1))],
+            [sg.Output(size=(32, 5))],
+            [sg.Button('Gerar senha')]
+        ]
+    
+        # Declarar a janela
+        
     def Iniciar(self):
         pass
     def salvar_senha(self):
         pass
+
+
+
+
+
 gen = PassGen()
 gen.Iniciar()
