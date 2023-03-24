633ad806836c95f75dd4b12279348205f7feb519
Junior Martini <erciliomartinijunior@gmail.com>
2023-03-24 15:23:44 -0400

    programa finalizado

--- password_gen.py
+++ password_gen.py
@@ -8,26 +8,40 @@ class PassGen:
         # layout
         sg.theme('Material2')
         layout = [
-            [sg.text('Site/Software', size=(10, 1)),
+            [sg.Text('Site/Software', size=(10, 1)),
              sg.Input(key='site', size=(20, 1))],
-            [sg.text('E-mail/Usuário', size=(10, 1)),
+            [sg.Text('E-mail/Usuário', size=(10, 1)),
              sg.Input(key='usuario', size=(20, 1))],
-            [sg.text('Quantidade de caracteres'), sg.Combo(values=list(
+            [sg.Text('Quantidade de caracteres'), sg.Combo(values=list(
                 range(30)),key='total_chars', default_value=1, size=(3, 1))],
             [sg.Output(size=(32, 5))],
             [sg.Button('Gerar senha')]
         ]
-    
-        # Declarar a janela
+        # Declarar janela
+        self.janela = sg.Window('Password Generator', layout)
         
     def Iniciar(self):
-        pass
-    def salvar_senha(self):
-        pass
+        while True:
+            evento, valores = self.janela.read()
+            if evento == sg.WINDOW_CLOSED:
+                break
+            if evento == 'Gerar senha':  # Correção aqui
+                nova_senha = self.gerar_senha(valores)
+                print(nova_senha)
+                self.salvar_senha(nova_senha, valores)
 
+    def gerar_senha(self, valores):
+        char_list = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$&*'
+        chars = random.choices(char_list, k=int(valores['total_chars']))
+        new_pass = ''.join(chars)
+        return new_pass
+    
+    def salvar_senha(self, nova_senha, valores):
+        with open('senhas.txt', 'a', newline='') as arquivo:
+            arquivo.write(
+                f"site: {valores['site']}, usuario: {valores['usuario']}, nova senha: {nova_senha}")
 
-
-
+        print('Arquivo Salvo')
 
 gen = PassGen()
 gen.Iniciar()
